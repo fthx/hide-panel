33675f3d001572f528bb12a78329508d4b08ff27
fthx <23138504+fthx@users.noreply.github.com>
2020-12-23 00:02:14 +0100

    Add files via upload

---
+++ extension.js
@@ -0,0 +1,50 @@
+/* 
+	Hide Panel
+	Copyright Francois Thirioux 2020
+	GitHub contributors: @fthx
+	License GPL v3
+*/
+
+const Main = imports.ui.main;
+const Panel = Main.panel;
+const Overview = Main.overview;
+const AppMenu = Main.panel.statusArea.appMenu;
+
+class Extension {
+    constructor() {
+    }
+    
+    _set_panel(height, opacity) {
+    	Panel.set_height(height);
+    	Panel.set_opacity(opacity);
+    }
+    
+    _show_panel() {
+    	AppMenu.hide();
+    	this._set_panel(this.panel_height, 255);
+    }
+    
+    _hide_panel() {
+    	AppMenu.show();
+    	this._set_panel(1, 0);
+    }
+
+    enable() {
+		this.panel_height = Panel.get_height();
+		this._set_panel(1, 0);
+		this.entering_overview = Overview.connect("showing", this._show_panel.bind(this));
+		this.leaving_overview = Overview.connect("hiding", this._hide_panel.bind(this));
+    }
+
+    disable() {
+    	Overview.disconnect(this.entering_overview);
+    	Overview.disconnect(this.leaving_overview);
+    	Panel.set_height(this.panel_height);
+    	this._set_panel(this.panel_height, 255);
+    }
+}
+
+function init() {
+	return new Extension();
+}
+
