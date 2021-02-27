e99253da11c94dafb3bda99c4885284ae19002d5
fthx <23138504+fthx@users.noreply.github.com>
2021-02-27 13:49:29 +0100

    Refine app menu hiding process

--- extension.js
+++ extension.js
@@ -1,14 +1,16 @@
 /* 
 	Hide Panel
-	Copyright Francois Thirioux 2020
+	Copyright Francois Thirioux 2021
 	GitHub contributors: @fthx
 	License GPL v3
 */
+
 
 const Main = imports.ui.main;
 const Panel = Main.panel;
 const Overview = Main.overview;
 const AppMenu = Main.panel.statusArea.appMenu;
+
 
 class Extension {
     constructor() {
@@ -20,26 +22,28 @@ class Extension {
     }
     
     _show_panel() {
-    	AppMenu.hide();
     	this._set_panel(this.panel_height, 255);
     }
     
     _hide_panel() {
-    	AppMenu.show();
     	this._set_panel(1, 0);
     }
 
     enable() {
+    	AppMenu.container.hide();
 		this.panel_height = Panel.get_height();
 		this._set_panel(1, 0);
-		this.entering_overview = Overview.connect("showing", this._show_panel.bind(this));
-		this.leaving_overview = Overview.connect("hiding", this._hide_panel.bind(this));
+		this.showing = Overview.connect('showing', this._show_panel.bind(this));
+		this.hiding = Overview.connect('hiding', this._hide_panel.bind(this));
     }
 
     disable() {
-    	Overview.disconnect(this.entering_overview);
-    	Overview.disconnect(this.leaving_overview);
+    	Overview.disconnect(this.showing);
+    	Overview.disconnect(this.hiding);
     	this._set_panel(this.panel_height, 255);
+    	if (!Main.sessionMode.isLocked) {
+    		AppMenu.container.show();
+    	}
     }
 }
 
